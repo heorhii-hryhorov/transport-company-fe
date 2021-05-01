a348b6fdeb69bb674dfa4d8e165d8a81e6cb5043
Heorhii Hryhorov <heorhiihryhorov@Heorhiis-MacBook-Pro.local>
2021-05-01 19:20:50 +0300

    added home layout

--- src/containers/HomeContainer/HomeContainer.tsx
+++ src/containers/HomeContainer/HomeContainer.tsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import {
+  Col, Container, Row, Table,
+} from 'reactstrap';
 
 const HomeContainer: React.FC = () => (
-
-  <h1>Home</h1>
-
+  <Container>
+    <Row className="mb-3">
+      <Col xs="12">
+        <h3>Available Transport</h3>
+        <Table bordered responsive>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Transport number</th>
+              <th>Model</th>
+              <th>Transport type</th>
+              <th>Purchase date</th>
+              <th>Mileage</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody />
+        </Table>
+      </Col>
+    </Row>
+    <Row>
+      <Col xs="12">
+        <h3>Available Routes</h3>
+        <Table bordered responsive>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Start City</th>
+              <th>End City</th>
+              <th>Distance</th>
+              <th>Departure date</th>
+              <th>Transport type</th>
+              <th>Revenue</th>
+              <th>Transport ID</th>
+              <th>Arrival date</th>
+            </tr>
+          </thead>
+          <tbody />
+        </Table>
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default HomeContainer;
